06bb99bd2594b9563f62c71dc6785368c80997b3
Nadeem <72926418+Kviksand@users.noreply.github.com>
2022-04-25 23:31:18 +0200

    fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
- 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  constructor(private router: Router ){
-
-  }
- 
-  ngOnInit() {
-
-  }
- 
-
   username: string = ""
   password: string = ""
 
-  onLogin(){
-    if(this.username == "user" && this.password == "user")
-    {
+  constructor(private router: Router) {}
+
+  ngOnInit() { }
+
+  onLogin() {
+    if (this.username == "user" && this.password == "user") {
       this.router.navigate(["/chat"])
     }
-    else{
+    else {
       alert("wrong")
     }
   }
 
- 
- 
-  }
+}
