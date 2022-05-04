043461709947ce9692b0ecfa10080bb6ae912396
Nadeem <72926418+Kviksand@users.noreply.github.com>
2022-05-04 21:05:04 +0200

    presence functionality added

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BehaviorSubject, filter, scan, Subscription, tap, toArray } from 'rxjs';
 import { Group } from '../models/Group';
 import { GroupChannel } from '../models/GroupChannel';
@@ -14,7 +14,9 @@ import { WebsocketService } from '../services/websocket.service';
   styleUrls: ['./chat.component.css']
 })
 
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
+  @ViewChild('channelHistory') private channelHistory : ElementRef | undefined
+
   selectedGroup?: Group
   selectedChannel?: GroupChannel
   text?: string
@@ -23,16 +25,30 @@ export class ChatComponent implements OnInit {
   isWsLoggedIn?: boolean
   groupChatMessages: GroupChatMsg[] = new Array<GroupChatMsg>()
   channelMessages: GroupChatMsg[] = new Array<GroupChatMsg>()
+  // has a map since it contains the jid plus the status of the user
+  // TODO: might implement statuses for users
+  groupUsersOnlineStatus: Map<string, string> = new Map<string, string>()
 
   private channelSubscription?: Subscription
   private groupChatMessageSubject = new BehaviorSubject<GroupChatMsg[] | null>(null)
   private selectedChannelSubject = new BehaviorSubject<GroupChannel | null>(null)
+  
 
   constructor(private websocketService: WebsocketService,
     private userDataService: UserDataService,
     private groupDataService: GroupDataService) { }
+  
+  
+  ngAfterViewChecked(): void {
+    // NOTE: Checks if there is an update/rerender on the page and if there is it will place the scroll bar in the bottom
+    if (this.channelHistory)
+    this.channelHistory.nativeElement.scrollTop = this.channelHistory?.nativeElement.scrollHeight
+    
+  }
 
   ngOnInit(): void {
+
+    // TODO: Replace "isLoggedIn" with method that can actually check if the connection has failed
     this.websocketService.isLoggedIn?.subscribe(isLoggedin => {
       this.isWsLoggedIn = isLoggedin
 
@@ -41,13 +57,15 @@ export class ChatComponent implements OnInit {
           this.groupChatMessages.push(msg)
           this.groupChatMessageSubject.next(this.groupChatMessages)
         })
-      }
+      } 
     })
 
     this.userDataService.userObservable.subscribe((user) => {
       if (user) {
         this.user = user
-        this.groupDataService.getGroup(this.user).subscribe(groups => this.groups = groups)
+        this.groupDataService.getGroup(this.user).subscribe(groups =>{
+           this.groups = groups
+          })
       }
     })
 
@@ -69,6 +87,23 @@ export class ChatComponent implements OnInit {
         }
       }
     )
+
+    this.websocketService.presenceObservable.subscribe(msg => {
+      console.log(msg)
+      
+
+      if(msg.item?.jid) {
+        if(msg.type == "") {
+          this.groupUsersOnlineStatus.set(msg.item.jid, 'online')
+        } else if (msg.type == "unavailable") {
+          this.groupUsersOnlineStatus.delete(msg.item.jid)
+        } else {
+          console.log(msg.type + ': is unknown')
+        }
+        console.log(this.groupUsersOnlineStatus)
+        
+      }
+    })
   }
 
   selectGroup(groupId: number) {
@@ -81,6 +116,7 @@ export class ChatComponent implements OnInit {
     if(this.selectedChannel) {
       this.selectedChannelSubject.next(this.selectedChannel)
     }
+    
   }
 
   removeSelected() {
@@ -102,9 +138,30 @@ export class ChatComponent implements OnInit {
   }
 
   getMemberNameFromJid(jid: string) {
+
     return this.selectedGroup?.members.find(
       member => member.chatInfo.jid == jid
       )?.contact.name
+  }
+
+  isMemberOnline(jid: string) {
+    if(this.groupUsersOnlineStatus.has(jid)) {
+      switch (this.groupUsersOnlineStatus.get(jid)) {
+        case 'online':
+          return true
+        default:
+          return false
+      }
+    }
+    return false
+  }
+
+  isOwnMessage(msg: GroupChatMsg) {
+    return msg.sender == this.user?.chatInfo.jid
+  }
+
+  loadMessages() {
+    return this.channelMessages
   }
 
 }
