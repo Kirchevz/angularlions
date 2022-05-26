3f65327a0745bf64d0f214e5163e3dc2e900e38c
Nadeem <72926418+Kviksand@users.noreply.github.com>
2022-05-26 15:59:55 +0200

    removed messaging service

--- src/app/mocks/Groups.ts
+++ src/app/mocks/Groups.ts
@@ -1,69 +1,69 @@
-// import { Group } from "../models/Group";
+import { Group } from "../models/Group";
 
-// export let GROUPS: Group[] = [
-//     {
-//         id: 1, name: "FirstGroup", jid: "nadeemo.FirstEvarGroup@conference.localhost", departmentId: 1, channels: [
-//             { id:1, name: "general", jid: "nadeemo.FirstEvarGroup.general@conference.localhost" },
-//             { id:2, name: "support", jid: "nadeemo.FirstEvarGroup.support@conference.localhost" }
-//         ], 
-//         members: [
-//             {
-//                 id: 1,
-//                 webUser: {username: "nadeemo", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "nadeem ab", email: "some@mail.com", birthday: new Date("2005-09-18")},
-//                 chatInfo: {jid: "user1@localhost", password: "passw0rd" },
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 1}
-//             },
-//             {
-//                 id: 2,
-//                 webUser: {username: "valentino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "valentin vt", email: "some1@mail.com", birthday: new Date("2001-02-13")},
-//                 chatInfo: {jid: "user2@localhost", password: "passw0rd" },
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 2}
+export let GROUPS: Group[] = [
+    {
+        id: 1, name: "FirstGroup", jid: "nadeemo.FirstEvarGroup@conference.localhost", departmentId: 1, channels: [
+            { id:1, name: "general", jid: "nadeemo.FirstEvarGroup.general@conference.localhost" },
+            { id:2, name: "support", jid: "nadeemo.FirstEvarGroup.support@conference.localhost" }
+        ], 
+        members: [
+            {
+                id: 1,
+                webUser: {username: "nadeemo", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "nadeem ab", email: "some@mail.com", birthday: new Date("2005-09-18")},
+                chatInfo: {jid: "user1@localhost", password: "passw0rd" },
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 1, signOutDate: null, signUpDate: new Date()}}
+            },
+            {
+                id: 2,
+                webUser: {username: "valentino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "valentin vt", email: "some1@mail.com", birthday: new Date("2001-02-13")},
+                chatInfo: {jid: "user2@localhost", password: "passw0rd" },
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 2, signOutDate: null, signUpDate: new Date()}}
                     
-//             },
-//             {
-//                 id: 4,
-//                 webUser: {username: "casperino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "casper s", email: "some1t@mail.com", birthday: new Date("1992-02-13")},
-//                 chatInfo: {jid: "user4@localhost", password: "passw0rd"},
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 6}
-//             },
-//         ]
-//     },
-//     {
-//         id: 2, name: "SecondGroup", jid: "nadeemo.Secundo@conference.localhost", departmentId: 1, channels: [
-//             { id:2, name: "general", jid: "nadeemo.Secundo.general@conference.localhost"}
-//         ],  members: [
-//             {
-//                 id: 1,
-//                 webUser: {username: "nadeemo", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "nadeem ab", email: "some@mail.com", birthday: new Date("2005-09-18")},
-//                 chatInfo: {jid: "user1@localhost", password: "passw0rd" },
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 1}
-//             },
-//             {
-//                 id: 2,
-//                 webUser: {username: "valentino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "valentin vt", email: "some1@mail.com", birthday: new Date("2001-02-13")},
-//                 chatInfo: {jid: "user2@localhost", password: "passw0rd" },
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 2}
+            },
+            {
+                id: 4,
+                webUser: {username: "casperino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "casper s", email: "some1t@mail.com", birthday: new Date("1992-02-13")},
+                chatInfo: {jid: "user4@localhost", password: "passw0rd"},
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 6, signOutDate: null, signUpDate: new Date()}}
+            },
+        ]
+    },
+    {
+        id: 2, name: "SecondGroup", jid: "nadeemo.Secundo@conference.localhost", departmentId: 1, channels: [
+            { id:2, name: "general", jid: "nadeemo.Secundo.general@conference.localhost"}
+        ],  members: [
+            {
+                id: 1,
+                webUser: {username: "nadeemo", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "nadeem ab", email: "some@mail.com", birthday: new Date("2005-09-18")},
+                chatInfo: {jid: "user1@localhost", password: "passw0rd" },
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 1, signOutDate: null, signUpDate: new Date()}}
+            },
+            {
+                id: 2,
+                webUser: {username: "valentino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "valentin vt", email: "some1@mail.com", birthday: new Date("2001-02-13")},
+                chatInfo: {jid: "user2@localhost", password: "passw0rd" },
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 2, signOutDate: null, signUpDate: new Date()}}
                     
-//             },
-//             {
-//                 id: 4,
-//                 webUser: {username: "casperino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
-//                 contact: {name: "casper s", email: "some1t@mail.com", birthday: new Date("1992-02-13")},
-//                 chatInfo: {jid: "user4@localhost", password: "passw0rd"},
-//                 tenantWebInfo: {CompanyId: 1, DepartmentId: 1, LeaseId: 6}
-//             },
-//         ] 
-//     },
-//     {
-//         id: 3, name: "ThirdGroup", jid: "nadeemo.Thirdo@conference.localhost", departmentId: 1, channels: [
-//             { id:2, name: "general", jid: "nadeemo.Thirdo.general@conference.localhost"}
-//         ],  members: []
-//     }
+            },
+            {
+                id: 4,
+                webUser: {username: "casperino", password: "passw0rd", isResident: true, isLoginPermitted: true, isChatActivate: true, hasResidentWebAccess: true, hasBoardWebAccess: false},
+                contact: {name: "casper s", email: "some1t@mail.com", birthday: new Date("1992-02-13")},
+                chatInfo: {jid: "user4@localhost", password: "passw0rd"},
+                tenantWebInfo: {company: {id: 1, name: "sad"}, department: {id:1, name: "what"}, lease: {id: 6, signOutDate: null, signUpDate: new Date()}}
+            },
+        ] 
+    },
+    {
+        id: 3, name: "ThirdGroup", jid: "nadeemo.Thirdo@conference.localhost", departmentId: 1, channels: [
+            { id:2, name: "general", jid: "nadeemo.Thirdo.general@conference.localhost"}
+        ],  members: []
+    }
     
     
-// ]
+]
